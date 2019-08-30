23c5bfb97e15cf1fb763f669c83b975591b84afe
Mo <Gabalyma@gmail.com>
2019-08-30 10:13:08 -0700

    Clean up

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var Excel = require("exceljs");
 
 var workbook = new Excel.Workbook();
 
+// Keywords to Search for
 var keywords = [
   "question",
+  "questions",
   "minute",
+  "minutes",
   "task",
+  "tasks",
   "workshop",
   "structural",
   "i-405",
@@ -13,21 +17,50 @@ var keywords = [
   "ug3",
   "ug4",
   "la brea",
+  "labrea",
   "hindry",
   "cos",
   "passage",
   "wall",
-  "slab"
+  "slab",
+  "memo",
+  "memos",
+  "rebar",
+  "cidh",
+  "west",
+  "soe",
+  "ug1",
+  "expo",
+  "greenline"
 ];
-var valuesarray = [];
+
+// Reading and overwriting the xlsx file
 workbook.xlsx.readFile("Prebid_HNTB_Review.xlsx").then(function() {
+  //The File name you want to access
+
+  // Worksheet by number
   var worksheet = workbook.getWorksheet(1);
+
+  // Column by number you want to go through
   var column = worksheet.getColumn(5);
   console.log(column.length);
+
+  // Going through the Cells
   column.eachCell({ includeEmpty: false }, function(cell, rowNumber) {
     var celltext = cell._value.value;
     if (celltext !== null) {
-      var cellwords = celltext.split(" ");
+      var cellwords = celltext
+        .split(" ")
+        .join(",")
+        .split(".")
+        .join(",")
+        .split(")")
+        .join(",")
+        .split("_")
+        .join(",")
+        .split("-")
+        .join(",")
+        .split(",");
       for (i = 0; i <= cellwords.length; i++) {
         if (cellwords[i] !== undefined) {
           var cellwordLc = cellwords[i].toLowerCase();
@@ -44,6 +77,7 @@ workbook.xlsx.readFile("Prebid_HNTB_Review.xlsx").then(function() {
       }
     }
   });
+  // Writing the new information to the file again
   workbook.xlsx.writeFile("Prebid_HNTB_Review.xlsx").then(function() {
     console.log("xls file is written.");
   });
